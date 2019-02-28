1fd66908934f1adc206d5798d0229a85aecb8799
Cattaneo Stefano <stefano@nearit.com>
2019-02-28 10:04:28 +0100

    update to 2.11.2

--- NearITSDK.framework/PrivateHeaders/NITCoupon.h
+++ NearITSDK.framework/PrivateHeaders/NITCoupon.h
@@ -17,11 +17,17 @@
 @property (nonatomic, strong) NSString *value;
 @property (nonatomic, strong) NSString *expiresAt;
 @property (nonatomic, strong) NSString *redeemableFrom;
-@property (nonatomic, strong) NSArray<NITClaim*> *claims;
+@property (nonatomic, strong) NSArray<NITClaim*> *claims DEPRECATED_ATTRIBUTE DEPRECATED_MSG_ATTRIBUTE("You should get claim info directly from coupon getters");
 @property (nonatomic, strong) NITImage *icon;
 @property (nonatomic, readonly) NSDate *expires;
 @property (nonatomic, readonly) NSDate *redeemable;
 @property (nonatomic, readonly) NSString *title;
+@property (nonatomic, readonly) NSString *serial;
+@property (nonatomic, readonly) NSString *claimedAt;
+@property (nonatomic, readonly) NSDate *claimed;
+@property (nonatomic, readonly) NSString *redeemedAt;
+@property (nonatomic, readonly) NSDate *redeemed;
+
 
 - (BOOL)hasContentToInclude;
 
--- NearITSDK.podspec
+++ NearITSDK.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
 s.name                  = 'NearITSDK'
-s.version               = '2.11.1'
+s.version               = '2.11.2'
 s.summary               = 'nearit.com iOS SDK'
 s.description           = 'nearit.com iOS SDK for Objective-C'
 
